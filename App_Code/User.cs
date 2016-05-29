d675ec5adee2d8343345cb1f8a097dbc0977c72b
ives <renhanlinbsl@163.com>
2016-05-29 20:41:19 +0800

    User Class inserUser
    sql insert----ives

---
+++ App_Code/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+/// <summary>
+/// User 的摘要说明
+/// </summary>
+public class User
+{
+	public User()
+	{
+
+	}
+    public bool insertUser(string username,string realname,string passsword,int department,int power)
+    {
+        //password加密
+        String strCon = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ToString();
+        MySqlConnection con= new MySqlConnection(strCon);
+        string strCmd = "insert into User (username,realname,password,department,power) values(@username)";
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand(strCmd, con);
+
+        MySqlParameter valueusername = new MySqlParameter("@username", MySqlDbType.VarChar);
+        valueusername.Value = username;
+        cmd.Parameters.Add(valueusername);
+
+
+        MySqlDataReader r = cmd.ExecuteReader();
+        bool result = r.Read();
+        con.Close();
+        if (result)
+            return true;
+        else
+            return false;     
+    }
+}
