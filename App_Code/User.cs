cb58a120065477c03f26a2f4214bf1fc52fb0754
ives <renhanlinbsl@163.com>
2016-05-29 22:44:43 +0800

    User Class hasUser
    write by ives.
    2016.5.29 22:44

--- App_Code/User.cs
+++ App_Code/User.cs
@@ -19,13 +19,29 @@ public class User
         //password加密
         String strCon = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ToString();
         MySqlConnection con= new MySqlConnection(strCon);
-        string strCmd = "insert into User (username,realname,password,department,power) values(@username)";
+        string strCmd = "insert into User (username,realname,password,department,power) values(@username,@realname,@password,@department,@power)";
         con.Open();
         MySqlCommand cmd = new MySqlCommand(strCmd, con);
 
         MySqlParameter valueusername = new MySqlParameter("@username", MySqlDbType.VarChar);
         valueusername.Value = username;
         cmd.Parameters.Add(valueusername);
+
+        MySqlParameter valuerealname = new MySqlParameter("@realname", MySqlDbType.VarChar);
+        valuerealname.Value = realname;
+        cmd.Parameters.Add(valuerealname);
+
+        MySqlParameter valuepassword = new MySqlParameter("@password", MySqlDbType.VarChar);
+        valuepassword.Value = passsword;
+        cmd.Parameters.Add(valuepassword);
+
+        MySqlParameter valuedepartment = new MySqlParameter("@department", MySqlDbType.Int16);
+        valuedepartment.Value = department;
+        cmd.Parameters.Add(valuedepartment);
+
+        MySqlParameter valuepower = new MySqlParameter("@power", MySqlDbType.Int16);
+        valuepower.Value = power;
+        cmd.Parameters.Add(valuepower);
 
 
         MySqlDataReader r = cmd.ExecuteReader();
@@ -36,4 +52,23 @@ public class User
         else
             return false;     
     }
+
+    public bool hasUser(string username)
+    {
+        //password加密
+        String strCon = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ToString();
+        MySqlConnection con = new MySqlConnection(strCon);
+        string strCmd = "select count(*) from User where username='"+username+"'";
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand(strCmd, con);
+        MySqlDataReader r = cmd.ExecuteReader();
+        r.Read();
+        string count = r[0].ToString();
+        con.Close();
+        if (Convert.ToInt32(count) == 0)
+            return false;
+        else
+            return true;
+    }
+
 }
